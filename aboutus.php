0900dfe8da4756d9974508e90c9fefe9c3ba4fb3
Malintha Nilhan Samarasinghe <86799359+MalinthaSamarasinghe@users.noreply.github.com>
2022-02-20 22:40:44 +0530

    Edited lines 19, 20, 21, 24, 25, 26, 29, 30, 31 & 32 by Malintha

--- aboutus.php
+++ aboutus.php
@@ -16,20 +16,17 @@ to make your home and your life super SMART.</p><br>";
 
 echo "<p class='updateInfo'><b>SMART SECURITY</b></p>";
 echo "<p class='updateInfo'>Smart-home monitoring products mean you can set up cameras to check on your home, even if you’re in another 
-country.<br>";
-echo "You can either watch a live stream through your phone or tablet, or just get alerts when the motion sensor is 
-triggered.<br>";
+country. You can either watch a live stream through your phone or tablet, or just get alerts when the motion sensor is triggered.<br>";
 
 echo "<p class='updateInfo'><b>SMART ENERGY</b></p>";
-echo "<p class='updateInfo'>Smart thermostat systems let you turn it off instantly from your phone – no matter where you are – or you can tell them<br>";
-echo "when you’re heading home to switch it back on, so it’s warm when you get in.<br>"; 
-echo "They can figure out your routine and customise your heating and hot water so it’s on when you need it and off when you don’t, helping to reduce your energy bills.<br>";
+echo "<p class='updateInfo'>Smart thermostat systems let you turn it off instantly from your phone – no matter where you are – or you can tell 
+them when you’re heading home to switch it back on, so it’s warm when you get in. They can figure out your routine and customise your heating 
+and hot water so it’s on when you need it and off when you don’t, helping to reduce your energy bills.<br>";
 
 echo "<p class='updateInfo'><b>SMART SPEAKERS</b></p>";
-echo "<p class='updateInfo'>We’ve all got too much to do.<br>"; 
-echo "Being able to command a virtual assistant to do something for you saves time, and it also means you can multitask more effectively.<br>"; 
-echo "If your hands are covered in flour but you need a two-minute timer, you can set it by voice without even having to stop kneading.<br>"; 
-echo "Because virtual assistants live in a speaker with a microphone, they’re present in your home whenever you need them.<br>"; 
+echo "<p class='updateInfo'>We’ve all got too much to do. Being able to command a virtual assistant to do something for you saves time, and it 
+also means you can multitask more effectively. If your hands are covered in flour but you need a two-minute timer, you can set it by voice without 
+even having to stop kneading. Because virtual assistants live in a speaker with a microphone, they’re present in your home whenever you need them.<br>"; 
 
 echo "<p class='updateInfo'>Whatever smart tech you are after, homteq has it on offer!</p>";
 
