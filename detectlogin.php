76434419edaf53d12cccc258f2fcbe9b172b4a5c
Malintha Nilhan Samarasinghe <86799359+MalinthaSamarasinghe@users.noreply.github.com>
2022-04-04 00:29:18 +0530

    Updated detectlogin.php file by Malintha

--- detectlogin.php
+++ detectlogin.php
@@ -3,8 +3,17 @@
 //if the session user id $_SESSION['userid'] is set (i.e. if the user has logged in successfully)
 if (isset($_SESSION['userid']))
 {
-	//display first name and surname on the right hand-side, right under the navigation bar
-	echo "<p style='float: right'>👤 ".$_SESSION['fname']." ".$_SESSION['sname']." | User type: ".$_SESSION['usertype']."</p><br>";
+	if ($_SESSION['usertype']== "c" || $_SESSION['usertype']== "C") //if user type is C, they are a customer
+	{ 
+		//display first name and surname on the right hand-side, right under the navigation bar
+		echo "<p style='float: right'>👤 ".$_SESSION['fname']." ".$_SESSION['sname']." | User Type: homteq Customer</p><br>";
+	}
+			
+	if ($_SESSION['usertype']=='A' || $_SESSION['usertype']== "a") //if user type is A, they are an admin
+	{ 
+		//display first name and surname on the right hand-side, right under the navigation bar
+		echo "<p style='float: right'>👤 ".$_SESSION['fname']." ".$_SESSION['sname']." | User type: homteq Administrator</p><br>";
+	}
 }
 
 ?>
